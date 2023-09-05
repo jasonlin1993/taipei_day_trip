2c399d508d81968a64b9d32d4a941708ecab2c56
jasonlin1993 <ninjachen.lin@gmail.com>
2023-09-05 16:53:06 +0800

    捷運列表切版 以及 javascript 左右點擊功能

---
+++ static/javascript/index.js
@@ -0,0 +1,12 @@
+function scrollToLeft() {
+    const container = document.getElementById('metroStations');
+    container.scrollLeft -= 300; 
+  }
+  
+  
+  function scrollToRight() {
+    const container = document.getElementById('metroStations');
+    container.scrollLeft += 300; // 可根據需要調整這個數值
+  }
+  
+  
