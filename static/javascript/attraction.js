d4a3e1c60e93fb358269a0f55ca5575fed6799a0
jasonlin1993 <ninjachen.lin@gmail.com>
2023-09-13 09:27:19 +0800

    Part 3-3: 完成訂購導覽中的時段選擇 點擊時切換導覽費用

--- static/javascript/attraction.js
+++ static/javascript/attraction.js
@@ -1,11 +1,18 @@
 function checkTime(timePeriod) {
     const morningBtn = document.getElementById('morningBtn');
     const afternoonBtn = document.getElementById('afternoonBtn');
+    const tourCostElement = document.getElementById('tourCost');
   
     const clickedImg = timePeriod === 'morning' ? morningBtn : afternoonBtn;
     const notClickedImg = timePeriod === 'morning' ? afternoonBtn : morningBtn;
   
     clickedImg.src = clickedImg.getAttribute('data-active-src');
     notClickedImg.src = notClickedImg.getAttribute('data-inactive-src');
+
+    if (timePeriod === 'morning') {
+        tourCostElement.innerText = '新台幣 2000 元';
+    } else {
+        tourCostElement.innerText = '新台幣 2500 元';
+    }
   }
   
